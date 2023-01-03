419479e88ed1b79141ee8fd83b0211a1b789131d
vivekkhade23 <khadev881@gmail.com>
2023-01-03 14:57:50 +0530

    job

---
+++ src/model/job.model.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+const jobSchema=new mongoose.Schema({
+    company:{type:String,required:true},
+    postedAt:{type:String,required:true},
+    city:{type:String,required:true},
+    location:{type:String,required:true},
+    role:{type:String,required:true},
+    level:{type:String,required:true},
+    contract:{type:String,required:true},
+    position:{type:String,required:true},
+    language:{type:String,required:true},
+
+})
+const jobModel=mongoose.model("job",jobSchema)
+
+module.exports=jobModel
---
+++ src/router/job.router.js
@@ -0,0 +1,60 @@
+ const express = require('express')
+const jobRouter=express.Router();
+const jobModel=require("../model/job.model")
+
+
+jobRouter.get("/",async(req,res)=>{
+  try {
+    let jobs= await jobModel.find({});
+  return  res.send(jobs)
+    
+  } catch (error) {
+    return res.send(error)
+  }
+})
+
+jobRouter.post("/joblisting",async(req,res)=>{
+    try {
+        const{company,city,location,role,level,contract,position,language}=req.body;
+        let date=new Date();
+        let timeStamp=date.toUTCString();
+        let profile=new jobModel({company,postedAt:timeStamp,city,location,role,level,contract,position,language})
+        profile=await profile.save();
+        return res.send('job created successfully')
+    } catch (e) {
+        return  res.send(e)
+    }
+})
+
+jobRouter.get("/jobsortasc",async(req,res)=>{
+    try {
+        let jobs= await jobModel.find({}).sort({postedAt:1});
+      return  res.send(jobs)
+        
+      } catch (error) {
+        return res.send(error)
+      }
+    
+})
+
+jobRouter.get("/jobsortdesc",async(req,res)=>{
+    try {
+        let jobs= await jobModel.find({}).sort({postedAt:-1});
+      return  res.send(jobs)
+        
+      } catch (error) {
+        return res.send(error)
+      }
+    
+})
+jobRouter.get("/jobfilter",async(req,res)=>{
+    try {
+        let jobs= await jobModel.find({level:"senior"});
+      return  res.send(jobs)
+        
+      } catch (error) {
+        return res.send(error)
+      }
+    
+})
+module.exports=jobRouter
